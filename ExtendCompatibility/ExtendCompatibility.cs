f90e9e4f113afb6e901d2ab13c66e5baa4df2845
eia485 <eia485git@gmail.com>
2021-09-14 11:48:51 -0700

    Add project files.

---
+++ ExtendCompatibility/ExtendCompatibility.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using System;
+using FrooxEngine;
+using FrooxEngine.LogiX;
+using BaseX;
+
+namespace ExtendCompatibility
+{
+	public class ExtendCompatibility : NeosMod
+	{
+		public override string Name => "ExtendCompatibility";
+		public override string Author => "eia485";
+		public override string Version => "1.0.0";
+		public override string Link => "https://github.com/EIA485/NeosExtendCompatibility/";
+		public override void OnEngineInit()
+		{
+			Harmony harmony = new Harmony("net.eia485.ExtendCompatibility");
+			harmony.PatchAll();
+		}
+
+		[HarmonyPatch(typeof(SyncElement))]
+		class ExtendCompatibilityPatch
+		{
+			[HarmonyFinalizer]
+			[HarmonyPatch("DecodeFull")]
+			static Exception FullFinalizer()
+			{
+				return null;
+			}
+
+			[HarmonyFinalizer]
+			[HarmonyPatch("DecodeDelta")]
+			static Exception DeltaFinalizer()
+			{
+				return null;
+			}
+		}
+	}
+}
